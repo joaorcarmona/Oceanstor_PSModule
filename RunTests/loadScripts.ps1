e3cdd016b7f32f1515b6a2383775b34132f9752f
Joao Carmona <joaor.carmona@gmail.com>
2022-05-19 12:29:04 +0100

    updated RunTests

--- RunTests/loadScripts.ps1
+++ RunTests/loadScripts.ps1
@@ -1,12 +1,17 @@
+
+$workDir = $(get-item $PSScriptRoot).Parent.FullName
 [array] $ModulesToLoad = @(
-    [pscustomobject]@{ModuleName='OceanStorPSModuleClass';Location='C:\users\jcarmona\Documents\Scripts\Main\OceanStorPSModuleClass.ps1'}
-    [pscustomobject]@{ModuleName='OceanStorPSModuleBase';Location='C:\users\jcarmona\Documents\Scripts\Main\OceanStorPSModuleBase.ps1'}
-    [pscustomobject]@{ModuleName='OceanStorPSModuleSys';Location='C:\users\jcarmona\Documents\Scripts\Main\OceanStorPSModuleSys.ps1'}
-    [pscustomobject]@{ModuleName='OceanStorPSModuleSAN';Location='C:\users\jcarmona\Documents\Scripts\Main\OceanStorPSModuleSAN.ps1'}
-    [pscustomobject]@{ModuleName='OceanStorPSModuleNAS';Location='C:\users\jcarmona\Documents\Scripts\Main\OceanStorPSModuleNAS.ps1'}
-    [pscustomobject]@{ModuleName='OceanStorPSModuleReport';Location='C:\users\jcarmona\Documents\Scripts\Main\OceanStorPSModuleReport.ps1'}
+    [pscustomobject]@{ModuleName="OceanStorPSModuleClass";Location="$workDir\Main\OceanStorPSModuleClass.ps1"}
+    [pscustomobject]@{ModuleName="OceanStorPSModuleBase";Location="$workDir\Main\OceanStorPSModuleBase.ps1"}
+    [pscustomobject]@{ModuleName="OceanStorPSModuleSys";Location="$workDir\Main\OceanStorPSModuleSys.ps1"}
+    [pscustomobject]@{ModuleName="OceanStorPSModuleSAN";Location="$workDir\Main\OceanStorPSModuleSAN.ps1"}
+    [pscustomobject]@{ModuleName="OceanStorPSModuleNAS";Location="$workDir\Main\OceanStorPSModuleNAS.ps1"}
+    [pscustomobject]@{ModuleName="OceanStorPSModuleReport";Location="$workDir\Main\OceanStorPSModuleReport.ps1"}
 )
 
 foreach ($module in $ModulesToLoad){
     . $module.Location
 }
+
+export-DMStorage -Hostname "" -ReportFile ""
+
