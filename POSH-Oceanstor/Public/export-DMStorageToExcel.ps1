f0ed0672f98ce82450e48241bb82631d909e9e92
Joao Carmona <joaor.carmona@gmail.com>
2022-05-22 04:26:34 +0100

    split in funcitons in single file; added ps1m and ps1d file

--- POSH-Oceanstor/Public/export-DMStorageToExcel.ps1
+++ POSH-Oceanstor/Public/export-DMStorageToExcel.ps1
@@ -1,44 +1,3 @@
-function export-OceanstorStorage{
-	<#
-	.SYNOPSIS
-		Function that returns an OceanStor Storage Device with all the properties currently in the module.
-
-	.DESCRIPTION
-		Function that returns an OceanStor Storage Device with all the properties currently in the module.
-		This object can be used to report or exported for documentation.
-		Is an export all storage configuration
-
-	.PARAMETER Hostname
-		is mandatory [string] parameter, that can be a hostname or an IP Address of the Huawei Oceanstor Device
-
-	.INPUTS
-
-	.OUTPUTS
-		returns the Huawei Oceanstor Device object with all the configuration. Return a Custom object
-
-	.EXAMPLE
-
-		PS C:\> $storageExport = export-OceanstorStorage -hostname storage.domain.tld
-
-	.NOTES
-		Filename: OceanstorPSModuleReport.ps1
-		Author: Joao Carmona
-		Modified date: 2022-05-22
-		Version 0.2
-
-	.LINK
-	#>
-	[Cmdletbinding()]
-	Param(
-		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,Position=0,Mandatory=$true)]
-			[String]$Hostname
-	)
-
-	$result = [OceanstorStorage]::new($Hostname)
-
-	return $result
-}
-
 function export-DMStorageToExcel{
 	<#
 	.SYNOPSIS
@@ -67,7 +26,7 @@ function export-DMStorageToExcel{
 		PS C:\> export-DMStorageToExcel -OceanStor $StorageDevice -ReportFile "c:\temp\StorageReport.xlsx"
 
 	.NOTES
-		Filename: OceanstorPSModuleReport.ps1
+		Filename: export-DMStorageToExcel.ps1
 		Author: Joao Carmona
 		Modified date: 2022-05-22
 		Version 0.2
@@ -86,7 +45,7 @@ function export-DMStorageToExcel{
 
 	if ($hostname -ne $null)
 	{
-		$storage = export-OceanstorStorage -Hostname $Hostname
+		$storage = export-DeviceManager -Hostname $Hostname
 	} else {
 		$storage = $OceanStor
 	}
