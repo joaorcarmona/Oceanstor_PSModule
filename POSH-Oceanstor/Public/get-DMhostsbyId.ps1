a932f4ffded5dabd1757f64b9b2a90a0d241e164
Joao Carmona <joaor.carmona@gmail.com>
2022-05-27 23:11:16 +0100

    renaming classes

---
+++ POSH-Oceanstor/Public/get-DMhostsbyId.ps1
@@ -0,0 +1,60 @@
+function get-DMhostsbyId{
+    <#
+.SYNOPSIS
+    To Get Huawei Oceanstor Storage configured Hosts querying by Hostid
+
+.DESCRIPTION
+    Function to request Huawei Oceanstor Storage configured Hosts
+
+.PARAMETER webSession
+    Optional parameter to define the session to be use on the REST call. If not defined, the "deviceManager" Global Variable will be used
+
+.PARAMETER webSession
+     parameter to define the session to be use on the REST call. If not defined, the "deviceManager" Global Variable will be used
+
+.INPUTS
+
+.OUTPUTS
+    returns the Huawei Oceanstor Storage configured Hosts in the system. Return an Array object.
+
+.EXAMPLE
+
+    PS C:\> get-DMhosts -webSession $session
+
+    OR
+
+    PS C:\> $hosts = get-DMhosts
+
+.NOTES
+    Filename: get-DMhosts.ps1
+    Author: Joao Carmona
+    Modified date: 2022-05-22
+    Version 0.2
+
+.LINK
+#>
+[Cmdletbinding()]
+Param(
+[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,Position=0,Mandatory=$false)]
+    [pscustomobject]$WebSession
+)
+
+if ($WebSession){
+    $session = $WebSession
+} else {
+    $session = $deviceManager
+}
+
+$response = invoke-DeviceManager -WebSession $session -Method "GET" -Resource "host" | Select-Object -ExpandProperty data
+$hosts = New-Object System.Collections.ArrayList
+
+foreach ($thost in $response)
+{
+    $hostobj = [OceanStorHost]::new($thost)
+    $hosts += $hostobj
+}
+
+$result = $hosts
+
+return $result
+}
