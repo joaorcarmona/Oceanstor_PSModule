6e9af228621431e8b821a4d0577762cdca5c4678
Joao Carmona <joaor.carmona@gmail.com>
2022-05-28 04:59:59 +0100

    clean code get-DMShares - single function for all shares

---
+++ POSH-Oceanstor/Public/get-DMShares.ps1
@@ -0,0 +1,73 @@
+function get-DMShares{
+	<#
+	.SYNOPSIS
+		To Get Huawei Oceanstor Storage Shares
+
+	.DESCRIPTION
+		Function to request Huawei Oceanstor Storage Shares
+
+	.PARAMETER webSession
+		Optional parameter to define the session to be use on the REST call. If not defined, the "deviceManager" Global Variable will be used
+
+	.PARAMETER shareType
+		Mamdatory paramter to define the Share Type to Query ("NFS","CIFS")
+
+	.INPUTS
+
+	.OUTPUTS
+		returns the Huawei Oceanstor Storage Shares
+
+	.EXAMPLE
+
+		PS C:\> get-DMShares -webSession $session -shareType CIFS
+
+		OR
+
+		PS C:\> $shares = get-DMShares -shareType NFS
+
+	.NOTES
+		Filename: get-DMShares.ps1
+		Author: Joao Carmona
+		Modified date: 2022-05-28
+		Version 0.2
+
+	.LINK
+	#>
+	[Cmdletbinding()]
+    Param(
+    [Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,Position=0,Mandatory=$false)]
+        [pscustomobject]$WebSession,
+	[Parameter(Position=1,Mandatory=$true)]
+        [ValidateSet("CIFS","NFS")]
+        [string]$shareType
+	)
+
+	if ($WebSession){
+        $session = $WebSession
+    } else {
+        $session = $deviceManager
+    }
+
+	switch ($shareType)
+	{
+		CIFS {$resourceQuery = "CIFSHARE"}
+		NFS {$resourceQuery = "NFSHARE"}
+	}
+
+    $response = invoke-DeviceManager -WebSession $session -Method "GET" -Resource $resourceQuery | Select-Object -ExpandProperty data
+    $shares = New-Object System.Collections.ArrayList
+
+	foreach ($tshare in $response)
+	{
+		switch ($shareType)
+		{
+			CIFS {$share = [OceanStorCIFSShare]::new($tshare)}
+			NFS {$share = [OceanStorNFSShare]::new($tshare)}
+		}
+
+		$shares += $share
+	}
+
+	$result = $share
+	return $result
+}
