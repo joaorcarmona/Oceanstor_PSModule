092f0eb85d6abfa001aec8fbc4af4e1807320b9d
Joao Carmona <joaor.carmona@gmail.com>
2022-06-11 00:20:47 +0100

    fix dynamic reporting

--- POSH-Oceanstor/Public/export-DMStorageToExcel.ps1
+++ POSH-Oceanstor/Public/export-DMStorageToExcel.ps1
@@ -52,12 +52,15 @@ function export-DMStorageToExcel{
 
 	#TODO Use SaveFileDialog Form to select file
 
+	$lunsReport = new-DMObjectReport -Object $storage.luns -ReportType luns
+	$hostsReport = new-DMObjectReport -Object $storage.hosts -ReportType hosts
+
 	Export-Excel $ReportFile -AutoSize -TableName System -InputObject $storage.system -WorksheetName "Basic System"
 	Export-Excel $ReportFile -AutoSize -TableName Disks -InputObject $storage.disks -WorksheetName "System Disks"
 	Export-Excel $ReportFile -AutoSize -TableName StoragePools -InputObject $storage.StoragePools -WorksheetName "Storage Pools"
-	Export-Excel $ReportFile -AutoSize -TableName Luns -InputObject $storage.Luns -WorksheetName "Luns"
+	Export-Excel $ReportFile -AutoSize -TableName Luns -InputObject $lunsReport -WorksheetName "Luns"
 	Export-Excel $ReportFile -AutoSize -TableName LunGroups -InputObject $storage.LunGroups -WorksheetName "Lun Groups"
-	Export-Excel $ReportFile -AutoSize -TableName Hosts -InputObject $storage.hosts -WorksheetName "Hosts"
+	Export-Excel $ReportFile -AutoSize -TableName Hosts -InputObject $hostsReport -WorksheetName "Hosts"
 	Export-Excel $ReportFile -AutoSize -TableName HostGroups -InputObject $storage.HostGroups -WorksheetName "Hosts Groups"
 	Export-Excel $ReportFile -AutoSize -TableName vStores -InputObject $storage.vStores -WorksheetName "System vStores"
 
