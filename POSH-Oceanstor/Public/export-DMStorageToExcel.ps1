5c681fb172b9f8e3b22aa4b8003177a220edb1d5
Joao Carmona <joaor.carmona@gmail.com>
2022-06-17 00:58:57 +0100

    Added feature dynamic report to Excel. Still not complete

--- POSH-Oceanstor/Public/export-DMStorageToExcel.ps1
+++ POSH-Oceanstor/Public/export-DMStorageToExcel.ps1
@@ -35,13 +35,38 @@ function export-DMStorageToExcel{
 	#>
 	[Cmdletbinding()]
 	Param(
-		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,Position=0,Mandatory=$true,ParameterSetName="NewConnection")]
+		[Parameter(ValueFromPipeline=$True,
+				ValueFromPipelineByPropertyName=$True,
+				Position=0,
+				Mandatory=$true,
+				ParameterSetName="NewConnection",
+				HelpMessage='Enter a Storage IP or FQDN.')]
+        	[Alias("Storage")]
 			[String]$Hostname,
-		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,Position=0,Mandatory=$true,ParameterSetName="CurrentConnection")]
+		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,Position=1,Mandatory=$true,ParameterSetName="CurrentConnection")]
 			[PSCustomObject]$OceanStor,
-		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,Position=0,Mandatory=$true)]
-			[string]$ReportFile
-	)
+		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,Position=2,Mandatory=$true)]
+			[string]$ReportFile,
+		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$false,Position=3,Mandatory=$false)]
+			[switch]$IncludeLuns = $false,
+		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$false,Position=4,Mandatory=$false)]
+			[switch]$IncludeHosts = $false,
+		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$false,Position=5,Mandatory=$false)]
+			[switch]$IncludeSystem = $false,
+		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$false,Position=6,Mandatory=$false)]
+			[switch]$IncludeDisks = $false,
+		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$false,Position=7,Mandatory=$false)]
+			[switch]$IncludeStoragePools = $false,
+		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$false,Position=8,Mandatory=$false)]
+			[switch]$IncludeLunGroups= $false,
+		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$false,Position=9,Mandatory=$false)]
+			[switch]$IncludeHostGroups= $false,
+		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$false,Position=10,Mandatory=$false)]
+			[switch]$IncludevStore= $false,
+		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$false,Position=11,Mandatory=$false)]
+			[switch]$FullReport= $false
+)
+
 
 	if ($hostname -ne "")
 	{
@@ -52,16 +77,58 @@ function export-DMStorageToExcel{
 
 	#TODO Use SaveFileDialog Form to select file
 
-	$lunsReport = new-DMObjectReport -Object $storage.luns -ReportType luns
-	$hostsReport = new-DMObjectReport -Object $storage.hosts -ReportType hosts
+	if ($FullReport -eq $true)
+	{
+		$IncludeLuns = $True
+		$IncludeHosts = $True
+		$IncludeSystem = $True
+		$IncludeDisks = $True
+		$IncludeStoragePools = $true
+		$IncludeLunGroups = $True
+		$IncludeHostGroups = $True
+		$IncludevStore = $True
+	}
 
-	Export-Excel $ReportFile -AutoSize -TableName System -InputObject $storage.system -WorksheetName "Basic System"
-	Export-Excel $ReportFile -AutoSize -TableName Disks -InputObject $storage.disks -WorksheetName "System Disks"
-	Export-Excel $ReportFile -AutoSize -TableName StoragePools -InputObject $storage.StoragePools -WorksheetName "Storage Pools"
-	Export-Excel $ReportFile -AutoSize -TableName Luns -InputObject $lunsReport -WorksheetName "Luns"
-	Export-Excel $ReportFile -AutoSize -TableName LunGroups -InputObject $storage.LunGroups -WorksheetName "Lun Groups"
-	Export-Excel $ReportFile -AutoSize -TableName Hosts -InputObject $hostsReport -WorksheetName "Hosts"
-	Export-Excel $ReportFile -AutoSize -TableName HostGroups -InputObject $storage.HostGroups -WorksheetName "Hosts Groups"
-	Export-Excel $ReportFile -AutoSize -TableName vStores -InputObject $storage.vStores -WorksheetName "System vStores"
+	if ($IncludeLuns -eq $True)
+	{
+		$lunsReport = new-DMObjectReport -Object $storage.luns -ReportType luns
+		Export-Excel $ReportFile -AutoSize -TableName Luns -InputObject $lunsReport -WorksheetName "Luns"
+	}
+
+	if ($IncludeHosts -eq $True)
+	{
+		$hostsReport = new-DMObjectReport -Object $storage.hosts -ReportType hosts
+		Export-Excel $ReportFile -AutoSize -TableName Hosts -InputObject $hostsReport -WorksheetName "Hosts"
+	}
+
+	if ($IncludeHostGroups -eq $True)
+	{
+		Export-Excel $ReportFile -AutoSize -TableName HostGroups -InputObject $storage.HostGroups -WorksheetName "Hosts Groups"
+	}
+
+	if ($IncludeSystem -eq $True)
+	{
+		Export-Excel $ReportFile -AutoSize -TableName System -InputObject $storage.system -WorksheetName "Basic System"
+	}
+
+	if ($IncludeDisks -eq $True)
+	{
+		Export-Excel $ReportFile -AutoSize -TableName Disks -InputObject $storage.disks -WorksheetName "System Disks"
+	}
+
+	If ($IncludeLunGroups -eq $True)
+	{
+		Export-Excel $ReportFile -AutoSize -TableName LunGroups -InputObject $storage.LunGroups -WorksheetName "Lun Groups"
+	}
+
+	If ($IncludeStoragePools -eq $True)
+	{
+		Export-Excel $ReportFile -AutoSize -TableName StoragePools -InputObject $storage.StoragePools -WorksheetName "Storage Pools"
+	}
+
+	if ($IncludevStore -eq $true)
+	{
+		Export-Excel $ReportFile -AutoSize -TableName vStores -InputObject $storage.vStores -WorksheetName "System vStores"
+	}
 
 }
