74219ff171cb85cf758247cf4a1f88c92d6e6164
Joao Carmona <joaor.carmona@gmail.com>
2022-06-17 01:10:46 +0100

    added HostGroups template

--- POSH-Oceanstor/Private/import-ReportTemplates.ps1
+++ POSH-Oceanstor/Private/import-ReportTemplates.ps1
@@ -4,3 +4,5 @@ $workDir = $(get-item $PSScriptRoot).Parent.FullName
 #Locattion for Lun Report Template
 $global:LunsReportTemplate  = $workDir + "/Templates/Report-Luns.xml"
 $global:HostsReportTemplate  = $workDir + "/Templates/Report-Hosts.xml"
+$global:HostGroupsReportTemplate  = $workDir + "/Templates/Report-HostGroups.xml"
+
