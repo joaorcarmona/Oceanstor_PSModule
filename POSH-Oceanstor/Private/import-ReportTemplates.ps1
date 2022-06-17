ad828db0df511273b9bc560a1d2a3bf1c7cf3c79
Joao Carmona <joaor.carmona@gmail.com>
2022-06-17 18:12:41 +0100

    generate disks custom report

--- POSH-Oceanstor/Private/import-ReportTemplates.ps1
+++ POSH-Oceanstor/Private/import-ReportTemplates.ps1
@@ -6,4 +6,5 @@ $global:LunsReportTemplate  = $workDir + "/Templates/Report-Luns.xml"
 $global:HostsReportTemplate  = $workDir + "/Templates/Report-Hosts.xml"
 $global:HostGroupsReportTemplate  = $workDir + "/Templates/Report-HostGroups.xml"
 $global:LunGroupsReportTemplate  = $workDir + "/Templates/Report-LunGroups.xml"
+$global:DisksReportTemplate  = $workDir + "/Templates/Report-Disks.xml"
 
