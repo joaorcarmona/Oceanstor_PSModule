75a78ad899ea0ed36bcd24d24ce6b572b9c56199
Joao Carmona <joaor.carmona@gmail.com>
2022-06-23 00:42:35 +0100

    Bond Port Class

---
+++ POSH-Oceanstor/Private/class-OceanstorPortBond.ps1
@@ -0,0 +1,47 @@
+class OceanStorPortBond{
+    [string]${Port Type}
+    [string]${Port Id}
+    [string]${Port Name}
+    [string]${health Status}
+    [string]${Running Status}
+    [string]${Ethernet Ports}
+    [string]${MTU}
+    [string]${Port Usage}
+    [string]${Device Name}
+
+    OceanStorPortBond ([array]$portReceived){
+        $this.{Port Type} = $portReceived.TYPE
+        $this.{Port Id} = $portReceived.ID
+        $this.{Port Name} = $portReceived.NAME
+
+        switch ($portReceived.HEALTHSTATUS)
+        {
+            0 {$this.{health Status} = "unknown"}
+            1 {$this.{health Status} = "normal"}
+            2 {$this.{health Status} = "faulty"}
+            3 {$this.{health Status} = "about to fail"}
+            9 {$this.{health Status} = "partially damaged"}
+        }
+
+        switch ($portReceived.RUNNINGSTATUS)
+        {
+            0 {$this.{Running Status} = "unknown"}
+            1 {$this.{Running Status} = "normal"}
+            2 {$this.{Running Status} = "running"}
+            10 {$this.{Running Status} = "link up"}
+            11 {$this.{Running Status} = "link down"}
+        }
+
+        $this.{Ethernet Ports} = $portReceived.PORTIDLIST
+        $this.{MTU} = $portReceived.MTU
+
+        switch ($portReceived.USEDTYPE)
+        {
+            1 {$this.{Port Usage} = "used for VM"}
+            2 {$this.{Port Usage} = "used for Storage"}
+        }
+        $this.{Device Name} = $portReceived.DEVICENAME
+
+    }
+
+}
