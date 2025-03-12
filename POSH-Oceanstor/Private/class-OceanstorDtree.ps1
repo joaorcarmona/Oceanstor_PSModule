109f78868daf5b8d74a894cd8f55f2cca9f9e650
joaorcarmona <joaor.carmona@gmail.com>
2025-03-12 12:35:40 +0000

    dtree function draft

---
+++ POSH-Oceanstor/Private/class-OceanstorDtree.ps1
@@ -0,0 +1,23 @@
+class OceanStorDtree {
+    #Define Properties
+    [string]$Id
+    [string]$Name
+    [string]$parentId
+    [string]${Quota Switch}
+    [string]${Quota Switch Status}
+    [string]${Share Type}
+    [bool]${Quota Configured}
+    [string]${vStore Id}
+    [string]${vStore Name}
+    [string]${Security Style}
+    [string]${Quota Count}
+    [string]${Replication Count}
+    [string]${Snapshot Count}
+    [string]${Locking Policy}
+    
+    OceanStorDtree ([array]$dtree)
+    {
+
+    }
+
+}
