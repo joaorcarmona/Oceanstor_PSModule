dc6bfbfe1c4de37a1e67250dc365869af7e81aa8
joaorcarmona <joaor.carmona@gmail.com>
2026-02-10 15:13:54 +0000

    Added new cmdlet to get BBU information from Huawei OceanStor storage systems. Updated existing cmdlets to support the new functionality and ensure compatibility with the latest API changes. This enhancement allows users to easily retrieve BBU status and information, improving the overall management of their storage systems. Improve session hanlding and security, by removing credentials from memory  - dont use credentials anymore after the first authentication

--- POSH-Oceanstor/Private/class-OceanstorSession.ps1
+++ POSH-Oceanstor/Private/class-OceanstorSession.ps1
@@ -3,7 +3,8 @@ class OceanstorSession{
 	hidden [string]$Hostname
 
 	#Define Host Credentials Property
-	hidden [System.Management.Automation.PSCredential]$Credentials
+	# dont need credential in the session, just use it to get the token and then discard it. Dont want to have the credentials in memory for long time
+	#hidden [System.Management.Automation.PSCredential]$Credentials 
 
 	#Define DeviceID Property
 	hidden [string]$DeviceId
@@ -21,13 +22,13 @@ class OceanstorSession{
 	hidden [string]$Version
 
     # Constructor
-    OceanstorSession ([PSCustomObject] $logonSession, [System.Collections.IDictionary]$SessionHeader, [Microsoft.PowerShell.Commands.WebRequestSession]$webSession, [string] $hostname, [System.Management.Automation.PSCredential]$credentials)
+    OceanstorSession ([PSCustomObject] $logonSession, [System.Collections.IDictionary]$SessionHeader, [Microsoft.PowerShell.Commands.WebRequestSession]$webSession, [string] $hostname)
     {
         $this.DeviceId = $logonsession.data.deviceid
         $this.WebSession = $WebSession
         $this.Headers = $SessionHeader
         $this.iBaseToken = $logonsession.data.iBaseToken
-        $this.Credentials = $credentials
+        #$this.Credentials = $credentials
         $this.Hostname = $hostname
 
 		$getDeviceManager = get-DMSystem -WebSession $this
