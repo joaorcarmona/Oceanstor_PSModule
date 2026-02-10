266796e2cc79cdcdea21694b366cdeb7460c0c36
joaorcarmona <joaor.carmona@gmail.com>
2026-02-10 17:13:47 +0000

    Added Description to the get-dmbbus commandlet

--- POSH-Oceanstor/Public/get-DMbbus.ps1
+++ POSH-Oceanstor/Public/get-DMbbus.ps1
@@ -1,4 +1,24 @@
 function get-dmbbus {
+    <#
+    .SYNOPSIS
+        To Get Huawei Oceanstor Storage System BBU
+    .DESCRIPTION
+        Function to request Huawei Oceanstor Storage System BBU
+    .PARAMETER webSession
+        Optional parameter to define the session to be use on the REST call. If not defined, the "deviceManager" Global Variable will be used
+    .INPUTS
+    .OUTPUTS
+        returns the Huawei Oceanstor Storage System BBU
+    .EXAMPLE
+
+        PS C:\> get-DMbbus -webSession $session
+
+        OR
+
+        PS C:\> $bbus = get-DMbbus
+    .NOTES
+        Filename: get-DMbbus.ps1
+    #>
     [Cmdletbinding()]
     Param(
     [Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,Position=0,Mandatory=$false)]
